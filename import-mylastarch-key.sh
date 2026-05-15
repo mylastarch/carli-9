ea29c8d54a68c7c3bf3bc6db0fb42ec5eeba92d5
mylastarch <mylastarch@dccathome.com>
2026-05-15 08:43:23 -0400

    changed key server

--- import-mylastarch-key.sh
+++ import-mylastarch-key.sh
@@ -2,8 +2,7 @@
 # shellcheck disable=SC2034
 
 echo "get the mylastarch key in"
-sudo pacman-key --recv-keys C8B25E49B0428123 --keyserver hkp://ipv4.pool.sks-keyservers.net:11371
-
+sudo pacman-key --recv-keys C8B25E49B0428123 --keyserver keyserver.ubuntu.com
 echo "Lsign the keys"
 sudo pacman-key --lsign-key C8B25E49B0428123
 
